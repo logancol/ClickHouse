2c5677f54735db354487796d7acd21b2edf1ade3
logancol <114114927+logancol@users.noreply.github.com>
2024-02-11 20:49:07 -0500

    Implementation of toIPv4 and toIPv6 functions
    This file implements two functions that allow for the mapping of IPV4 addresses to IPV6 format and back. This conversion makes it so that an IPV4 address can be compared to an IPV6 in a query (as requested in issue ClickHouse#59849).

--- src/Functions/ipComp.h
+++ src/Functions/ipComp.h
@@ -3,17 +3,18 @@
 #include <string>
 
 
-std::string toIPv4(const std::string& IPv6){
-//maps given IPV6 address to IPV4 mapping
+std::string toIPv4(const std::string & IPv6)
+{
+///IPV6 address mapped to IPV4 format
 std::string temp = IPv6.substr(7);
 return temp;
 }
 
-std::string toIPv6(const std::string& IPv4){
-//maps given IPV4 address to IPV6 mapping
+std::string toIPv6(const std::string & IPv4)
+{
+///IPV4 address mapped to IPV6 format
 std::string temp = "::ffff:" + IPv4;
 return temp;
 }
 
-//example
-//toIPv6(192.0.2.146) would yield ::ffff:192.0.2.146, allowing this IPV4 formatted address to be compared or processed with an IPV6
+///toIPv6(192.0.2.146) would yield ::ffff:192.0.2.146, allowing this IPV4 formatted address to be compared or processed with an IPV6
